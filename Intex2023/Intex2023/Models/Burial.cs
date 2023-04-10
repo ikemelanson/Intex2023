d177c8c57bd955d53d3b517a94437a5dce866766
oliviamorganstl <95643022+oliviamorganstl@users.noreply.github.com>
2023-04-10 14:52:55 -0600

    burial records + supervised pages

---
+++ Intex2023/Intex2023/Models/Burial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex2023.Models
+{
+    public partial class Burial
+    {
+        [Key]
+        [Required]
+        public int BookID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Classification { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public int PageCount { get; set; }
+        [Required]
+        public float Price { get; set; }
+
+
+    }
+}
