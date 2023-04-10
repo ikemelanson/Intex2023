0ce7a455d4f9f9a51102bf2f501089a396319c69
oliviamorganstl <95643022+oliviamorganstl@users.noreply.github.com>
2023-04-10 15:44:46 -0600

    Half broken cookie

--- Intex2023/Intex2023/Program.cs
+++ Intex2023/Intex2023/Program.cs
@@ -2,11 +2,6 @@ using Intex2023.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-//Adding things in for cookies
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -34,53 +29,6 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
-//Adding things in for cookie consent
-
-app.UseCookiePolicy();
-app.Use(async (context, next) =>
-{
-    if (!context.Request.Cookies.ContainsKey("consent"))
-    {
-        context.Response.Cookies.Append("consent", "false", new CookieOptions
-        {
-            Expires = DateTime.Now.AddYears(1),
-            SameSite = SameSiteMode.Strict,
-            Secure = true
-        });
-        context.Response.Redirect("/consent");
-    }
-    else if (context.Request.Cookies["consent"] == "false")
-    {
-        context.Response.Redirect("/consent");
-    }
-    else
-    {
-        await next();
-    }
-});
-
-app.Map("/consent", consentApp =>
-{
-    consentApp.Run(async context =>
-    {
-        await context.Response.WriteAsync("<h1>Cookie Consent</h1>");
-        await context.Response.WriteAsync("<p>This website uses cookies to improve your experience. Click the button below to give your consent.</p>");
-        await context.Response.WriteAsync("<form method=\"post\"><button type=\"submit\">Give Consent</button></form>");
-        if (context.Request.Method == "POST")
-        {
-            context.Response.Cookies.Delete("consent");
-            context.Response.Cookies.Append("consent", "true", new CookieOptions
-            {
-                Expires = DateTime.Now.AddYears(1),
-                SameSite = SameSiteMode.Strict,
-                Secure = true
-            });
-            context.Response.Redirect("/");
-        }
-    });
-});
 
 
 
--- Intex2023/Intex2023/wwwroot/js/cookie-consent.js
+++
@@ -1,8 +0,0 @@
-﻿window.addEventListener("DOMContentLoaded", function () {
-    var consent = document.querySelector(".cookie-consent");
-    var acceptButton = consent.querySelector("button");
-    acceptButton.addEventListener("click", function () {
-        document.cookie = "consent=true; expires=Thu, 01 Jan 2099 00:00:00 UTC; path=/";
-        consent.style.display = "none";
-    });
-});
