901df02ff320bb2d86216f5304c850dab093d8ea
oliviamorganstl <95643022+oliviamorganstl@users.noreply.github.com>
2023-04-12 16:18:02 -0600

    tech stack/starter burials

--- Intex2023/Intex2023/Controllers/HomeController.cs
+++ Intex2023/Intex2023/Controllers/HomeController.cs
@@ -45,15 +45,15 @@ namespace Intex2023.Controllers
 
             return View(x);
         }
+        public IActionResult IndividualBurial()
+        {
+            return View();
+        }
         public IActionResult Supervised()
         {
             return View();
         }
         public IActionResult Unsupervised()
-        {
-            return View();
-        }
-        public IActionResult Tech()
         {
             return View();
         }
