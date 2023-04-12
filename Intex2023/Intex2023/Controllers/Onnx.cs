0ac4aa365a9431c254f05b14b038b11f7f9733be
ikemelanson <ikemelanson@gmail.com>
2023-04-12 16:56:34 -0600

    add onnx

---
+++ Intex2023/Intex2023/Controllers/Onnx.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Intex2023.Models;
+
+
+namespace Intex2023.Controllers
+{
+
+    [ApiController]
+    [Route("/score")]
+    public class Onnx : ControllerBase
+	{
+
+        private InferenceSession _session;
+
+        public Onnx(InferenceSession session)
+        {
+            _session = session;
+        }
+
+        [HttpPost]
+        public ActionResult Score(MummyData data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+            });
+            Tensor<string> score = result.First().AsTensor<string>();
+            var prediction = new Prediction { PredictedValue = score.First()};
+            result.Dispose();
+            return Ok(prediction);
+        }
+    }
+}
+
---
+++ Intex2023/Intex2023/Models/MummyData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace Intex2023.Models
+{
+	public class MummyData
+	{
+		public float squarenorthsouth { get; set; }
+        public float depth { get; set; }
+        public float westtohead { get; set; }
+        public float westtofeet { get; set; }
+        public float sex_M { get; set; }
+        public float sex_Unknown { get; set; }
+        public float eastwest_W { get; set; }
+        public float adultsubadult_A { get; set; }
+        public float adultsubadult_C { get; set; }
+        public float ageatdeath_A { get; set; }
+        public float ageatdeath_C { get; set; }
+        public float ageatdeath_I { get; set; }
+        public float ageatdeath_N { get; set; }
+
+        public Tensor<float> AsTensor()
+        {
+            float[] data = new float[]
+            {squarenorthsouth,
+            depth,
+            westtohead,
+            westtofeet,
+            sex_M,
+            sex_Unknown,
+            eastwest_W,
+            adultsubadult_A,
+            adultsubadult_C,
+            ageatdeath_A,
+            ageatdeath_C,
+            ageatdeath_I,
+            ageatdeath_N
+            };
+            int[] dimensions = new int[] { 1, 13 };
+            return new DenseTensor<float>(data, dimensions);
+        }
+    }
+}
+
