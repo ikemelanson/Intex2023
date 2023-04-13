724d16488b1de427a520703a4c0de6f3d2ddb0df
Devon Wolsleger <devonrw@me.com>
2023-04-12 20:23:39 -0600

    Head Direction

---
+++ Intex2023/Intex2023/Components/HeadDirectionViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Intex2023.Models;
+//Manages the category page
+namespace Intex2023.Components
+{
+    public class HeadDirectionViewComponent : ViewComponent
+    {
+
+        private IBurialRepository repo { get; set; }
+        public HeadDirectionViewComponent(IBurialRepository temp)
+        {
+            repo = temp;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedEstimateStature = RouteData?.Values["headdirection"];
+            var headdirection = repo.burialmain
+                .Select(x => x.headdirection)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(headdirection);
+        }
+    }
+}
