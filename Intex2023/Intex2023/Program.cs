b497c19c24e57ce08b63dafcc8ee18d6c3fcd097
Devon Wolsleger <devonrw@me.com>
2023-04-13 18:48:20 -0600

    Cookies

--- Intex2023/Intex2023/Program.cs
+++ Intex2023/Intex2023/Program.cs
@@ -57,7 +57,7 @@ builder.Services.Configure<IdentityOptions>(options =>
 
 builder.Services.AddSingleton<InferenceSession>(
   new InferenceSession("wwwroot/supervised.onnx")
-);//
+);
 
 var app = builder.Build();
 
